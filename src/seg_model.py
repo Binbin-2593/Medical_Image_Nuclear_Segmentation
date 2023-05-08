e63ca6f77c8e83018b1c93e3ec377239d86445fe
Binbin-2593 <82641728+Binbin-2593@users.noreply.github.com>
2023-05-08 21:37:23 +0800

    Update seg_model.py

--- src/seg_model.py
+++ src/seg_model.py
@@ -123,12 +123,17 @@ class SegModel:
         self.device = torch.device(
             'cuda:0' if torch.cuda.is_available() else 'cpu')
         self.model = self._setup_resnet()
-
+         """
         if path_to_pretrained_model:
             self.model.load_state_dict(torch.load(
                 path_to_pretrained_model
             ))
-
+        """
+        if path_to_pretrained_model:
+            self.model = torch.load(
+                path_to_pretrained_model, map_location=self.device
+            )
+        
         (self.train_transform,self.val_transform) = self._setup_transform()
 
     def train(
